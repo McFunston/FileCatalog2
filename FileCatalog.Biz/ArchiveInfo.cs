410f45af528392f10a087c93b2fa02d09a0f56e8
McFunston <mica.funston@gmail.com>
2016-03-02 15:22:40 -0500

    Create ArchiveSet object.

---
+++ FileCatalog.Biz/ArchiveInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCatalog.Biz
+{
+    class ArchiveInfo
+    {
+        public string Name { get; set; }
+        public DriveType DriveType { get; set; }
+        public string Label { get; set; }
+
+        public ArchiveInfo(string name, DriveType driveType, string label)
+        {
+            Name = name;
+            DriveType = driveType;
+            Label = label;
+        }
+    }
+}
