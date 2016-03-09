bbf6aad7b3ae84924fb140173f9cf2c4608090a6
McFunston <mica.funston@gmail.com>
2016-03-09 09:54:16 -0500

    Add basic unit test for ArchiveInfo class.

---
+++ FileCatalog.BizTests/ArchiveInfoTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileCatalog.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCatalog.Biz.Tests
+{
+    [TestClass()]
+    public class ArchiveInfoTests
+    {
+        [TestMethod()]
+        public void ArchiveInfoTest()
+        {
+            //Arrange
+            var archiveInfo = new ArchiveInfo("test", System.IO.DriveType.Fixed, "nolabel");
+            string expectedName = "test";
+            System.IO.DriveType expectedMediaType = System.IO.DriveType.Fixed;
+            string expectedlabel = "nolabel";
+            //Act
+
+            //Assert
+            Assert.AreEqual(expectedName, archiveInfo.Name);
+            Assert.AreEqual(expectedMediaType, archiveInfo.DriveType);
+            Assert.AreEqual(expectedlabel, archiveInfo.Label);
+        }
+    }
+}
