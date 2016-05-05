3e89a62e0c3692be11e66bf4f631e04fdd23a052
McFunston <mica.funston@gmail.com>
2016-05-05 15:02:58 -0400

    Add Catalog class with FileSet property which is a dictionary of <Catalog, List<File>>.

---
+++ FileCatalog.Biz/Catalog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCatalog.Biz
+{
+    public class Catalog
+    {
+        public Dictionary<Archive, List<IFile>> FileSet { get; set; }
+        public Catalog(Archive archive, List<IFile> files)
+        {
+            FileSet.Add(archive, files);
+        }
+    }
+}
