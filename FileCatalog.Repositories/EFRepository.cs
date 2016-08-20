cb1a1d57183c9b2cffb1927c5631012f07180d4e
McFunston <mica.funston@gmail.com>
2016-08-19 20:49:25 -0400

    Create EFRepository

---
+++ FileCatalog.Repositories/EFRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCatalog.Repositories
+{
+    class EFRepository : IRepository
+    {
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Search(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
