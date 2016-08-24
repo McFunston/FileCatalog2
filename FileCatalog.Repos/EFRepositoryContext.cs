2d9df267f3320cba62c9dd710f524eb83b4d8708
McFunston <mica.funston@gmail.com>
2016-08-23 20:49:19 -0400

    Create Common project for common Data types. Create EFRepositoryContext: DbContext

---
+++ FileCatalog.Repos/EFRepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using FileCatalog.Biz;
+using System.Data.Entity;
+
+namespace FileCatalog.Repositories
+{
+    class EFRepositoryContext: DbContext
+    {
+        public EFRepositoryContext(): base()
+        {
+
+        }
+
+        public DbSet<Catalog> Catalogs { get; set; }
+    }
+}
---
+++ FileCatalog.Repositories/EFRepositoryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCatalog.Repositories
+{
+    class EFRepositoryContext: DbContext
+    {
+        public EFRepositoryContext(): base()
+        {
+
+        }
+
+        //public DbSet<Catalog> Catalogs { get; set; }
+    }
+}
